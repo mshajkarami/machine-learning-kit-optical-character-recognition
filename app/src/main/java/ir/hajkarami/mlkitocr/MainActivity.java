995eb8f4b7ade3d03c4eda80ac5f9db843aefa8c
mshajkarami <mshajkarami.dev@gmail.com>
2025-04-18 11:07:04 +0330

    feat: Implement image selection and text recognition
    This commit adds functionality to select an image from the gallery and perform text recognition using ML Kit. It includes:
    
    - Implementation of `OpenGallery()` to allow users to pick an image.
    - Handling of the selected image in `onActivityResult()` to display it in the ImageView and process it with the ML Kit TextRecognizer.
    - Implementation of `ProcessText()` to extract and display recognized text from the image.
    - Added Glide dependency to load image into ImageView
    - Added `onPause` to prevent crash

--- app/src/main/java/ir/hajkarami/mlkitocr/MainActivity.java
+++ app/src/main/java/ir/hajkarami/mlkitocr/MainActivity.java
@@ -1,27 +1,42 @@
 package ir.hajkarami.mlkitocr;
 
+import android.app.ComponentCaller;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
 import com.google.mlkit.vision.text.TextRecognition;
 import com.google.mlkit.vision.text.TextRecognizer;
 import com.google.mlkit.vision.text.TextRecognizerOptions;
 
+import java.io.IOException;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int PICK_IMAGE = 123;
     ImageView imageView;
     TextView textTv;
     Button speechBtn, imageBtn;
@@ -60,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
         textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
-                if (status  != TextToSpeech.ERROR)
+                if (status != TextToSpeech.ERROR)
                     textToSpeech.setLanguage(Locale.US);
             }
         });
@@ -74,6 +89,75 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void OpenGallery() {
+        Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        getIntent.setType("image/");
+        Intent pickIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        pickIntent.setType("image/");
+        Intent choserIntent = Intent.createChooser(getIntent, "Select Image");
+        choserIntent.putExtra(Intent.EXTRA_ALTERNATE_INTENTS, new Intent[]{pickIntent});
+        startActivityForResult(choserIntent, PICK_IMAGE);
+    }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data, @NonNull ComponentCaller caller) {
+        super.onActivityResult(requestCode, resultCode, data, caller);
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK) {
+            if (data != null) {
+                byte[] byteArray = new byte[0];
+                String path = null;
+                try {
+                    inputImage = InputImage.fromFilePath(this, data.getData());
+                    Bitmap resultUri = inputImage.getBitmapInternal();
+                    Glide.with(this).load(resultUri).into(imageView);
+                    // process image
+                    Task<Text> result =
+                            recognizer.process(inputImage)
+                                    .addOnSuccessListener(new OnSuccessListener<Text>() {
+                                        @Override
+                                        public void onSuccess(@NonNull Text text) {
+                                            ProcessText(text);
+                                        }
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+    private void ProcessText(Text text) {
+        // start processing text
+        String resultText = text.getText();
+        for (Text.TextBlock block : text.getTextBlocks()) {
+            String blockText = block.getText();
+            textTv.append("\n");
+            Point[] blockCornerPoint = block.getCornerPoints();
+            Rect blockFrame = block.getBoundingBox();
+            for (Text.Line line : block.getLines()) {
+                String lineText = line.getText();
+                Point[] lineCornerPoints = line.getCornerPoints();
+                Rect lineFrame = line.getBoundingBox();
+                for (Text.Element element : line.getElements()) {
+                    textTv.append(" ");
+                    String elementText = element.getText();
+                    textTv.append(elementText);
+                    Point[] elementCornerPoints = element.getCornerPoints();
+                    Rect elementFrame = element.getBoundingBox();
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        if (!textToSpeech.isSpeaking()){
+            super.onPause();
+        }
     }
 }
