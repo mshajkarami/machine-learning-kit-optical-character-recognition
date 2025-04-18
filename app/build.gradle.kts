0e5528fc8f38478ca2c05e025625df277c5bc3dc
mshajkarami <mshajkarami.dev@gmail.com>
2025-04-18 10:38:01 +0330

    feat: Implement basic layout and ML Kit text recognition
    This commit introduces the basic layout for the application, including an ImageView, TextView, and two Buttons. It also implements the initial setup for ML Kit text recognition, including initializing the TextRecognizer and handling image selection from the gallery. Additionally, a TextToSpeech component is initialized to read out the recognized text.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    // add any other libraries needed for our app
+    implementation ("com.google.android.gms:play-services-mlkit-text-recognition:16.2.0")
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
 }
--- app/src/main/java/ir/hajkarami/mlkitocr/MainActivity.java
+++ app/src/main/java/ir/hajkarami/mlkitocr/MainActivity.java
@@ -1,6 +1,12 @@
 package ir.hajkarami.mlkitocr;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +14,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+import com.google.mlkit.vision.text.TextRecognizerOptions;
+
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    TextView textTv;
+    Button speechBtn, imageBtn;
+
+    InputImage inputImage;
+    TextRecognizer recognizer;
+    TextToSpeech textToSpeech;
+    public Bitmap textImage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +42,38 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+        imageView = findViewById(R.id.imageView);
+        textTv = findViewById(R.id.textTv);
+        speechBtn = findViewById(R.id.read_text);
+        imageBtn = findViewById(R.id.choice_image);
+
+        imageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenGallery();
+                textImage = imageView.getDrawingCache();
+                inputImage = InputImage.fromBitmap(textImage, 0);
+            }
+        });
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status  != TextToSpeech.ERROR)
+                    textToSpeech.setLanguage(Locale.US);
+            }
+        });
+        speechBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textToSpeech.speak(textTv.getText().toString(), TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
+
+    }
+
+    private void OpenGallery() {
+
     }
 }
